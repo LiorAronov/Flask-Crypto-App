85159c0ebbd50b9d8cb430f2dee838c3bef65cbe
LiorAronov <lioraronov@gmail.com>
2023-06-01 13:25:39 +0300

    upply staten.

--- infrastructure/dev/backend.tf
+++ infrastructure/dev/backend.tf
@@ -2,12 +2,12 @@
 # bucket = "remote-state-currency-flask-app-1-6-23"
 # dynamodb_table = "locking-state-currency-flask-app-1-6-23"
 
-# terraform {
-#   backend "s3" {
-#     bucket = "remote-state-currency-flask-app-1-6-23"
-#     key = "crypto-currency-flask-app-project/terraform.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "locking-state-currency-flask-app-1-6-23"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "remote-state-currency-flask-app-1-6-23"
+    key = "crypto-currency-flask-app-project/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "locking-state-currency-flask-app-1-6-23"
+    encrypt = true
+  }
+}
