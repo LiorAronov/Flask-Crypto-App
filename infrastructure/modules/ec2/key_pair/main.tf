b48d9a384e58773ecda7d979d2fd62eef32e9e16
LiorAronov <lioraronov@gmail.com>
2023-06-01 13:14:01 +0300

    name fix

--- infrastructure/modules/ec2/key_pair/main.tf
+++ infrastructure/modules/ec2/key_pair/main.tf
@@ -6,12 +6,12 @@ resource "tls_private_key" "rsa" {
 
 # Public key - store in AWS vonsole.
 resource "aws_key_pair" "public_terrform_key" {
-  key_name   = var.public_key_pair_name
+  key_name   = var.public_key_name
   public_key = tls_private_key.rsa.public_key_openssh
 }
 
 # Private key - store in local file.
 resource "local_file" "private_terrform_key" {
   content  = tls_private_key.rsa.private_key_pem
-  filename = "../../../private_terrform_key/${var.private_key_pair_name}.pem"  
+  filename = "../../../private_terrform_key/${var.private_key_name}.pem"  
 }
