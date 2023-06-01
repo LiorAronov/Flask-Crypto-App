f7dbe6d8c0c431d7372631869abf4aafa25ac11f
LiorAronov <lioraronov@gmail.com>
2023-06-01 13:16:36 +0300

    dev - backend file.

---
+++ infrastructure/dev/backend.tf
@@ -0,0 +1,13 @@
+# Setting The Remote State And Locking State.
+# bucket = "remote-state-currency-flask-app-1-6-23"
+# dynamodb_table = "locking-state-currency-flask-app-1-6-23"
+
+# terraform {
+#   backend "s3" {
+#     bucket = "remote-state-currency-flask-app-1-6-23"
+#     key = "crypto-currency-flask-app-project/terraform.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "locking-state-currency-flask-app-1-6-23"
+#     encrypt = true
+#   }
+# }
